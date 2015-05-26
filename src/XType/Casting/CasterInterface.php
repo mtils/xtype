bd1cb3e706308df20e4c831bdb1835a12fd3a2e9
Michael Tils <mtils@web-utils.de>
2015-05-26 20:56:36 +0200

    Added typecaster interface

---
+++ src/XType/Casting/CasterInterface.php
@@ -0,0 +1,29 @@
+<?php namespace XType\Casting;
+
+/**
+  * A Caster casts data between the model (application) and the user
+  * This should be on an atomic level like Datetime, Phone Numbers,
+  * adding units to amounts. This is not meant to be a template enginge...
+  **/
+interface CasterInterface
+{
+
+    /**
+     * Cast some value from user input to model data
+     * e.g. '08/20/2014' => \DateTime
+     *
+     * @param mixed (string)
+     * @return mixed
+     **/
+    public function toModel($userdata);
+
+    /**
+     * Cast some value suitable to show it to a user
+     * e.g. DateTime => '20.05.2015'
+     *
+     * @param mixed $modelData
+     * @return string
+     **/
+    public function toView($modelData);
+
+}
